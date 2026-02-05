b3feb7d77ceb0f966a40bdff1127a1ea4076be79
Luke <ljluke91@gmail.com>
2026-02-05 00:50:52 +0000

    Added straight logic, full house, 4 of a kind

--- play_hand.py
+++ play_hand.py
@@ -1,6 +1,7 @@
 from deck import Deck, Card
 from board import Board
 import time
+from winning_hands import best_hand_name
 
 #--------------------------
 # Create and shuffle the deck
@@ -11,7 +12,7 @@ deck.shuffle()
 
 board = Board()
 
-# Deal 2 hole cards
+# # Deal 2 hole cards
 player_hand = deck.deal(2)
 print("Your cards:", player_hand[0], "and", player_hand[1])
 
@@ -44,3 +45,28 @@ print(
 from winning_hands import best_hand_name
 all_cards = player_hand + board.cards
 print("You have", best_hand_name(all_cards))
+
+# --------------------------------------------------------
+## Rigged hand temporary testing straight
+
+# player_hand = [Card("♣", "4"), Card("♠", "5")]
+# board_cards = [Card("♥", "J"), Card("♦", "A"), Card("♠", "10"), Card("♥", "2"), Card("♣", "3")]
+
+# all_cards = player_hand + board_cards
+
+# print("Your cards:", player_hand[0], "and", player_hand[1])
+# print("The board:", board_cards[0], board_cards[1], board_cards[2], board_cards[3], board_cards[4])
+# print("You have", best_hand_name(all_cards))
+# -----------------------------------------------------------------------------------
+
+## Rigged hand temporary testing quads
+
+# player_hand = [Card("♥", "A"), Card("♠", "A")]
+# board_cards = [Card("♦", "A"), Card("♣", "A"), Card("♠", "K"), Card("♦", "10"), Card("♣", "5")]
+
+# all_cards = player_hand + board_cards
+
+# print("Your cards:", player_hand[0], "and", player_hand[1])
+# print("The board:", board_cards[0], board_cards[1], board_cards[2], board_cards[3], board_cards[4])
+# print("You have", best_hand_name(all_cards))
+# ---------------------------------------------------------------------------------------
--- winning_hands.py
+++ winning_hands.py
@@ -13,17 +13,68 @@ HAND_RANKS = {
     "Straight Flush": 9,
     "Royal Flush": 10
     }
+RANK_TO_VALUE = {
+    "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10,
+    "J": 11, "Q": 12, "K": 13, "A": 14
+}
 def best_hand_name(cards):
     # Count suits
     suit_counts = {}
+    rank_counts = {}
+    for card in cards:
+        rank = card.rank
+        rank_counts[rank] = rank_counts.get(rank, 0) + 1
+    print(rank_counts)
+
+    values = []
+    for card in cards:
+        values.append(RANK_TO_VALUE[card.rank])
+
+    values = sorted(set(values))
+
+    # Making the ace be 1 or 14 for high/low straights
+    if 14 in values:
+        values.append(1)
+        values = sorted(set(values))
 
     for card in cards:
         suit = card.suit
         suit_counts[suit] = suit_counts.get(suit, 0) + 1
 
+    ## Defining checks for hand ranks
+
+        counts = list(rank_counts.values())
+
+    # Check for four of a kind
+    if 4 in counts:
+        return "four of a kind"
+    # Check for a full house
+    if (3 in counts and 2 in counts) or (counts.count(3) >= 2):
+         return "a full house"
     # Check for flush
     for suit in suit_counts:
         if suit_counts[suit] >= 5:
             return "a flush"
+        
+    # Check for a straight
+    run = 1
+    for i in range(1, len(values)):
+        if values[i] == values[i - 1] + 1:
+            run += 1
+            if run >= 5:
+                return "a straight"
+        else:
+            run = 1
+    
+    # Check for 3 of a kind
+    if 3 in counts:
+        return "three of a kind"
+    # Check for two pair
+    if counts.count(2) == 2:
+        return "two pair"
+    #  Check for one pair
+    if 2 in counts:
+        return "one pair"
+        
 
     return "High Card"
